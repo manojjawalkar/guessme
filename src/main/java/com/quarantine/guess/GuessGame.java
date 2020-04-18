38a958de306e2611b8beacc900c846108b9d85e1
ruturaj <ruturaj.balwadkar@gmail.com>
2020-04-18 16:17:06 +0530

    Guessing Modified.

--- src/main/java/com/quarantine/guess/GuessGame.java
+++ src/main/java/com/quarantine/guess/GuessGame.java
@@ -3,6 +3,8 @@ package com.quarantine.guess;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class GuessGame {
     private Player player[];
     int guessME;
@@ -11,26 +13,28 @@ public class GuessGame {
 
     public void startGame(){
         int numOfPlayers =1;
-        System.out.println("Enter the number of players ");
+        System.out.println("Enter the number of players:");
         try{
             Scanner sc = new Scanner(System.in);
             numOfPlayers = sc.nextInt();
         } catch (Exception e){
             System.out.println("oops! that's not a number");
-            System.exit(0);
+            exit(0);
         }
 
         setUpPlayers(numOfPlayers);
         System.out.println("Players are ready to play");
         System.out.println("Game started!");
+        System.out.println("Guess any number till 50..");
+        guessME = (int) (Math.random() *50);
         while(true){
-            guessME = (int) (Math.random() *10);
+
             System.out.println("----------------\n" +
                     "Computer making a guess\n\tcomputer guessed = "+guessME);
             checkGuess();
             if(flag == 0) {
                 System.out.println("The winner is "+winner + "\nCorrect guess was ="+guessME);
-                System.exit(0);
+                exit(0);
             }
         }
     }
@@ -47,11 +51,26 @@ public class GuessGame {
     }
     public void checkGuess(){
         for (int i = 0; i < player.length; i++) {
+            int guess=player[i].makeGuess();
 //            System.out.print(player[i].getName()+ " make a guess ");
-            if(player[i].makeGuess() == guessME){
+            if(guess == guessME){
                 winner = player[i].getName();
+                System.out.println(winner+" "+"Congrats!!");
+                exit(0);
                 flag = 0;
             }
+            else if(guess < guessME)
+            {
+                System.out.println("Guess is low");
+
+            }
+            else if(guess > guessME)
+            {
+                System.out.println("Guess is high");
+            }
+
         }
+
+
     }
 }
